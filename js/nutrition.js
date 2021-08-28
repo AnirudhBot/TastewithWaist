b626b2d5867822ba27cdcb582254842245bc78a4
Anirudh Sharma <anirudh1304@gmail.com>
2021-08-28 16:07:48 +0530

    some js optimized

--- js/nutrition.js
+++ js/nutrition.js
@@ -8,12 +8,12 @@ let nutritionResult = document.querySelector(".nutrition-result");
 searchIcon.addEventListener( "click", () => {
     let searchValue = searchQuery.value ;
     console.log(searchValue);
-    if(searchValue != null) {
+    if(searchValue != false) {
         searchValue = searchValue.replace(" ", "%20");
         fetch(`https://api.edamam.com/api/nutrition-data?app_id=0ae7c4e8&app_key=f1dd039acb04ab641f23e1b03a37ed45&nutrition-type=cooking&ingr=${searchValue}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data);
+            if(data.calories !=0) {
             let html = "";
             html += `<table class="nutrition-table">
                         <tr class="nutrition-rows">
@@ -37,7 +37,10 @@ searchIcon.addEventListener( "click", () => {
                     </table>`
 
             nutritionResult.innerHTML = html;
+            }
+            else alert('Sorry! No data found');
         })    
     }
+    else alert('Enter some input');
 
 })
